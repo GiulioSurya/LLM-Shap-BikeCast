d7119227c53ccb1412e9947d67a25a100aee7cee
GiulioSurya <153427674+GiulioSurya@users.noreply.github.com>
2025-03-26 15:43:27 +0100

    Add files via upload

--- models.py
+++ models.py
@@ -1,33 +1,20 @@
 import os
+import warnings
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split,GridSearchCV
 import xgboost as xgb
+import bisect
 import pickle
-
-
-def save_metrics_excel(dtf_data, metrics, file_path, file_name):
-    """
-    Funzione per salvare un DataFrame in un file Excel.
-    """
-    df = dtf_data.copy()
-
-    if not isinstance(df, pd.DataFrame):
-        raise TypeError("Errore: 'df' deve essere un DataFrame Pandas.")
-    if not isinstance(file_path, str):
-        raise TypeError("Errore: 'file_path' deve essere una stringa.")
-    if not isinstance(file_name, str):
-        raise TypeError("Errore: 'file_name' deve essere una stringa.")
-
-    df = df[metrics]
-    full_file_path = os.path.join(file_path, file_name)
-    df.to_excel(full_file_path, index=False)
-    print(f"File salvato con successo in: {full_file_path}")
-
+from preprocessing import Preprocessing
 
 
 class Model(object):
-
     model = None
+    test_size = 0.2
+    target_col = "cnt"
+    file_path = r"C:\Users\loverdegiulio\Desktop"
+    random_state = 42
 
     """
     class base astratta che definisce il train, predict , save  e caricamento di un modello
@@ -36,37 +23,87 @@ class Model(object):
     def __init__(self):
         pass
 
-    def train(self, dtf_data, target_col, test_size, random_state):
-        pass
-
     @staticmethod
-    def grid_search(dtf_data,target_col,grid_params ,file_path, file_name, estimator=None ,scoring = None, metrics = None):
-
-        if estimator is None:
-            raise ValueError("Errore: scegliere un modello per estimatore")
-        if scoring is None:
-            raise ValueError("Errore: scegliere una loss function per scoring")
+    def save_metrics_excel(dtf_data, metrics, file_path, file_name):
+        """
+        Funzione per salvare un DataFrame in un file Excel.
+        """
         df = dtf_data.copy()
 
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("Errore: 'df' deve essere un DataFrame Pandas.")
+        if not isinstance(file_path, str):
+            raise TypeError("Errore: 'file_path' deve essere una stringa.")
+        if not isinstance(file_name, str):
+            raise TypeError("Errore: 'file_name' deve essere una stringa.")
+
+        df = df[metrics]
+        full_file_path = os.path.join(file_path, file_name)
+        df.to_excel(full_file_path, index=False)
+        return print (f"File salvato in: {full_file_path}")
+
+    @classmethod
+    def get_model(cls):
+        return  NotImplementedError("Il metodo get_model deve essere implementato nelle sottoclassi.")
+
+    @classmethod
+    def grid_search(cls,dtf_data,grid_params, file_name, target_col = target_col, file_path = file_path , scoring = None, metrics = None):
+        estimator = cls.get_model()
+
+        if scoring is None:
+            raise ValueError("Errore: scegliere una loss function per scoring")
+
+        df = dtf_data.copy()
         y = df[target_col]
         x = df.drop(target_col, axis=1)
 
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=cls.test_size, random_state=42)
 
         grid_search = GridSearchCV(estimator=estimator, param_grid=grid_params, scoring= scoring, cv=3,
                                  verbose=1, n_jobs=-1, return_train_score=True)
         grid_search.fit(x_train, y_train, eval_set=[(x_test, y_test)], verbose=True)
         df_result = pd.DataFrame(grid_search.cv_results_)
 
-        return save_metrics_excel(df_result, metrics, file_path, file_name)
+        if metrics is None:
+            metrics = df_result.columns.tolist()
+            warnings.warn("nessuna metrica selezionata, verranno restituite tutte le metriche disponibili, usare il parametro metrics per selezionare le colonne desiderate.")
+
+        return cls.save_metrics_excel(df_result, metrics, file_path, file_name)
+
+    def train(self, dtf_data, target_col = target_col, test_size = test_size, random_state = random_state):
+        df = dtf_data.copy()
+
+
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("Errore XgBoost.train: 'dtf_data' deve essere un DataFrame Pandas.")
+        if target_col not in df.columns:
+            raise ValueError(f"Errore XgBoost.train: la colonna target '{target_col}' non è presente nel DataFrame.")
+        if not (0 < test_size < 1):
+            raise ValueError("Errore XgBoost.train: 'test_size' deve essere un float compreso tra 0 e 1.")
+        if not isinstance(random_state, int):
+            raise TypeError("Errore XgBoost.train: 'random_state' deve essere un intero.")
+
+        y = df[target_col]
+        x = df.drop(target_col, axis=1)
+
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=random_state)
+        self.model.fit(x_train, y_train, eval_set=[(x_test, y_test)], verbose=True)
+
+        return self.model
 
     def predict(self, x):
 
         if self.model is None:
             raise AttributeError("non è possibile eseguire predict se il modello non è stato addestrato, eseguire prima train")
+        if not isinstance(x, pd.DataFrame):
+            raise TypeError("Errore Model.predict: 'x' deve essere un DataFrame Pandas.")
+        if x.shape[1] == 0:
+            raise ValueError("Errore Model.predict: il DataFrame di input non contiene colonne")
+
+        return self.model.predict(x)
 
 
-    def save(self, filepath):
+    def save(self, filepath = file_path):
         """
         Serializza e salva l’intero oggetto Model (self) su disco.
         """
@@ -79,7 +116,7 @@ class Model(object):
         print(f"Modello salvato in: {filepath}")
 
     @staticmethod
-    def load(filepath):
+    def load(filepath = file_path):
         """
         Carica l’intero oggetto Model precedentemente salvato con pickle.
         """
@@ -101,64 +138,21 @@ class XgBoost(Model):
         super().__init__()
         self.model_params = params
 
-        # if not (isinstance(n_estimators, int) and n_estimators > 0):
-        #     raise ValueError("Errore XgBoost: 'n_estimators' deve essere un intero positivo.")
-        # if not (isinstance(max_depth, int) and max_depth > 0):
-        #     raise ValueError("Errore XgBoost: 'max_depth' deve essere un intero positivo.")
-        # if not (isinstance(learning_rate, float) and learning_rate > 0):
-        #     raise ValueError("Errore XgBoost: 'learning_rate' deve essere un float > 0.")
-        # if not (isinstance(min_child_weight, (int, float)) and min_child_weight > 0):
-        #     raise ValueError("Errore XgBoost: 'min_child_weight' deve essere un numero > 0.")
-        # if not (isinstance(subsample, float) and 0 < subsample <= 1):
-        #     raise ValueError("Errore XgBoost: 'subsample' deve essere un float in (0,1].")
-        # if not (isinstance(colsample_bynode, float) and 0 < colsample_bynode <= 1):
-        #     raise ValueError("Errore XgBoost: 'colsample_bynode' deve essere un float in (0,1].")
-        # if not (isinstance(reg_lambda, float) and reg_lambda >= 0):
-        #     raise ValueError("Errore XgBoost: 'reg_lambda' deve essere un float >= 0.")
-
     @classmethod
-    def grid_search(cls,dtf_data,target_col,grid_params ,file_path, file_name, estimator= None,scoring=None, metrics = None):
-        df = dtf_data.copy()
-        if scoring is None:
-            scoring = "neg_root_mean_squared_error"
-        if estimator is None:
-            estimator = xgb.XGBRegressor(objective='reg:squarederror', random_state=42, early_stopping_rounds=1, enable_categorical= True)
-        if metrics is None:
-            metrics = df.columns
-        return super().grid_search(df, target_col, grid_params, file_path, file_name, estimator=estimator, scoring=scoring, metrics=metrics)
+    def get_model(cls, **kwargs):
+        return xgb.XGBRegressor(objective='reg:squarederror', random_state=42, early_stopping_rounds=10, enable_categorical= True, **kwargs)
 
 
-    def train(self, dtf_data, target_col = "cnt", test_size=0.2, random_state=42):
-        df = dtf_data.copy()
+    def train(self, dtf_data, target_col=None, test_size=None, random_state=None):
 
-        self.model = xgb.XGBRegressor(**self.model_params, enable_categorical=True, early_stopping_rounds=10)
+        if target_col is None:
+            target_col = self.target_col
+        if test_size is None:
+            test_size = self.test_size
+        if random_state is None:
+            random_state = self.random_state
 
-        if not isinstance(df, pd.DataFrame):
-            raise TypeError("Errore XgBoost.train: 'dtf_data' deve essere un DataFrame Pandas.")
-        if target_col not in df.columns:
-            raise ValueError(f"Errore XgBoost.train: la colonna target '{target_col}' non è presente nel DataFrame.")
-        if not (0 < test_size < 1):
-            raise ValueError("Errore XgBoost.train: 'test_size' deve essere un float compreso tra 0 e 1.")
-        if not isinstance(random_state, int):
-            raise TypeError("Errore XgBoost.train: 'random_state' deve essere un intero.")
+        self.model = self.get_model(**self.model_params)
 
+        return super().train(dtf_data, target_col=target_col, test_size=test_size, random_state=random_state)
 
-        y = df[target_col]
-        x = df.drop(target_col, axis=1)
-
-        x_train, x_test,y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=random_state)
-        self.model.fit(x_train, y_train, eval_set=[(x_test, y_test)], verbose = True)
-
-        return self.model
-
-    def predict(self, x):
-        super().predict(x)
-
-        # qua devo mettere il controllo per model, model deve essere non none
-        if not isinstance(x, pd.DataFrame):
-            raise TypeError("Errore XgBoost.predict: 'x' deve essere un DataFrame Pandas.")
-        if x.shape[1] == 0:
-            raise ValueError("Errore XgBoost.predict: il DataFrame di input non contiene colonne.")
-
-
-        return self.model.predict(x)
--- usage.py
+++ usage.py
@@ -4,8 +4,8 @@ from sklearn.model_selection import train_test_split,GridSearchCV
 from preprocessing import Preprocessing
 from models import XgBoost
 
-df_test = pd.read_csv("hour.csv")
 
+df_test = pd.read_csv("hour.csv")
 df_train, df_predict = train_test_split(df_test, test_size=0.2, random_state=42)
 # ----------------- pre processing dati
 preproc = Preprocessing()
@@ -17,7 +17,7 @@ df_train_encod  = preproc.transform_data(df_train, "train")
 df_test_encod = preproc.transform_data(df_predict, "predict")
 df_test = df_test_encod.copy()
 df_test_encod.drop(columns=["cnt"], inplace=True)
-# ------------------- stima griglia
+#------------------- stima griglia
 param_grid = {
     'n_estimators': [400],
     'max_depth': [7],
@@ -36,10 +36,7 @@ columns = [
     "split0_train_score", "split1_train_score", "split2_train_score",
     "mean_train_score", "std_train_score"
 ]
-
-# you must change file patch and file name
-XgBoost.grid_search(df_train_encod, target_col="cnt", grid_params=param_grid, file_path=r"C:\Users\loverdegiulio\Desktop", file_name="risultati.xlsx", metrics=columns)
-
+XgBoost.grid_search(df_train_encod, grid_params=param_grid, file_name="risultati.xlsx", scoring="neg_mean_squared_error", metrics=columns)
 # #train dataset
 params = {
 'n_estimators': 400,
@@ -49,21 +46,23 @@ params = {
 "subsample": 0.75,
 "colsample_bynode": 0.7,
 "reg_lambda": 0.5}
-
 model = XgBoost(params)
-model.train(df_train_encod, target_col="cnt", test_size=0.2, random_state=42)
-#model.save("training/xgboost_model.pkl")
-#
-#
-# # Ricarico da file pkl
-# loaded_model = XgBoost.load("training/xgboost_model.pkl")
+model.train(df_train_encod)
 predicted = model.predict(df_test_encod)
-
-
+#
+# model = XgBoost(params)
+# model.train(df_train_encod, target_col="cnt", test_size=0.2, random_state=42)
+# #model.save("training/xgboost_model.pkl")
+# #
+# #
+# # # Ricarico da file pkl
+# # loaded_model = XgBoost.load("training/xgboost_model.pkl")
+# predicted = model.predict(df_test_encod)
 #test with df
 #rmse
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 rmse = np.sqrt(mean_squared_error(df_test["cnt"], predicted))
 mae = mean_absolute_error(df_test["cnt"], predicted)
+
 
