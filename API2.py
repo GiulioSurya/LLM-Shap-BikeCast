0c61e710a421ce4e14c8f64e23a751ed7a723646
GiulioSurya <153427674+GiulioSurya@users.noreply.github.com>
2025-04-07 12:11:34 +0200

    api with fastapi and uvicorn + schemas for params

--- API2.py
+++ API2.py
@@ -1,116 +1,17 @@
-import sys
-from pathlib import Path
+import os.path
 from modello.preprocessing import Preprocessing
 import pandas as pd
 from modello.modell import XgBoost
-from pydantic import BaseModel, Field
-from fastapi import FastAPI
-from typing import Dict, List, Optional, Union
+from fastapi import HTTPException, FastAPI
+from modello.schemas import GridSearchParams, TrainingParams, PredictionParams
 
 api = FastAPI()
 
-
-class GridSearchParams(BaseModel):
-    data_grid_path: Optional[Union[str, Path]] = Field(
-        default=r"C:\Users\loverdegiulio\PycharmProjects\PythonProject1\modello\train.csv",
-        title="Percorso ai dati per il Grid Search",
-    )
-    grid_parameters: Dict[str, List[Union[int, float]]] = Field(
-        ...,
-        title="Parametri per il Grid Search",
-    )
-    file_name: str = Field(
-        ...,
-        title="Nome del file per il Grid Search",
-    )
-    scoring: str = Field(
-        ...,
-        title="Nome della metrica di scoring",
-    )
-    target_col: Optional[str] = Field(
-        default="cnt",
-        title="Nome della colonna target",
-    )
-    metrics: Optional[List[str]] = Field(
-        default=None,
-        title="Metriche da calcolare durante il Grid Search",
-    )
-    save_path: Optional[Union[str, Path]] = Field(
-        default=r"C:\Users\loverdegiulio\PycharmProjects\PythonProject1\training",
-        title="Percorso per il salvataggio del file",
-    )
-    kwargs: Optional[dict] = Field(
-        default=dict,
-        title="Parametri aggiuntivi per il Grid Search",
-    )
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "data_grid_path": r"C:\Users\loverdegiulio\PycharmProjects\PythonProject1\modello\train.csv",
-                    "grid_parameters": {
-                        "n_estimators": [400],
-                        "max_depth": [7],
-                        "learning_rate": [0.05],
-                        "min_child_weight": [65],
-                        "subsample": [0.7],
-                        "colsample_bynode": [0.7],
-                        "reg_lambda": [0.5]
-                    },
-                    "file_name": "grid_search_results.xlsx",
-                    "scoring": "neg_root_mean_squared_error",
-                    "target_col": "cnt",
-                    "metrics": [
-                        "mean_fit_time", "std_fit_time", "mean_score_time", "std_score_time",
-                        "param_colsample_bynode", "param_learning_rate", "param_max_depth",
-                        "param_min_child_weight", "param_n_estimators", "param_reg_lambda",
-                        "param_subsample", "params", "split0_test_score", "split1_test_score",
-                        "split2_test_score", "mean_test_score", "std_test_score", "rank_test_score",
-                        "split0_train_score", "split1_train_score", "split2_train_score",
-                        "mean_train_score", "std_train_score"
-                    ],
-                    "save_path": r"C:\Users\loverdegiulio\PycharmProjects\PythonProject1\training",
-                    "kwargs": {
-                        "cv": 3,
-                        "n_jobs": -1,
-                        "verbose": 1,
-                        "early_stopping_rounds": 10
-                    }
-                }
-            ]
-        }
-    }
-
-
-class TrainingParams(BaseModel):
-    data_train_path: Union[str, Path] = Field(
-        ..., title="Percorso ai dati per il training"
-    )
-    model_parameter: Dict[str, List[float]] = Field(
-        ..., title="Parametri per il training"
-    )
-    target: Optional[str] = Field(
-        default="cnt", title="Nome della colonna target"
-    )
-    kwargs: Optional[dict] = Field(
-        default=None, title="Parametri aggiuntivi per il training"
-    )
-
-
-class PredictionParams(BaseModel):
-    data_pred_path: Union[str, Path] = Field(
-        ..., title="Percorso ai dati per la predizione"
-    )
-    target: Optional[str] = Field(
-        default=None, title="Nome della colonna target"
-    )
-
-
-
-
 @api.post("/grid_search")
 def grid_search(params: GridSearchParams):
+
+    if not os.path.exists(params.data_grid_path):
+        raise HTTPException(status_code=404, detail=f"Il percorso {params.data_grid_path} non esiste.")
 
     dtf_grid = pd.read_csv(params.data_grid_path)
     preproc = Preprocessing()
@@ -123,8 +24,43 @@ def grid_search(params: GridSearchParams):
         target_col=params.target_col,
         scoring=params.scoring,
         metrics=params.metrics,
-        **(params.kwargs or dict)
+        **(params.kwargs or dict())
     )
+
+@api.post("/training")
+def training(params: TrainingParams):
+
+    dtf_train = pd.read_csv(params.data_train_path)
+
+    preprocessor = Preprocessing()
+    dtf_train_cln = preprocessor.transform_data(dtf_train, "train")
+
+    model_fit = XgBoost(model_parameters=params.model_parameter,
+                        target_col=params.target,
+                        **(params.kwargs or dict())
+                        )
+    model_fit.train(dtf_train_cln)
+    model_fit.save(params.save_model)
+
+@api.post("/predict")
+def predict(params: PredictionParams):
+
+    dtf_prect = pd.read_csv(params.data_pred_path)
+
+    preprocessor = Preprocessing()
+    preprocessor.transform_data(pd.read_csv(r"C:\Users\loverdegiulio\PycharmProjects\PythonProject1\modello\train.csv"), "train")
+
+    dtf_prect_cln = preprocessor.transform_data(dtf_prect, "predict")
+    dtf_prect_cln.drop(columns=[params.target], inplace=True)
+
+    model_fit = XgBoost().load(params.load_model)
+    model_pred = model_fit.predict(dtf_prect_cln)
+
+    return model_pred.tolist()
+
+
+
+
 
 
 
